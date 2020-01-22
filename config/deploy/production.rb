d326bcaaf09fda420e7ee7e6381de15d69aaa14f
omorihirofumi <kanazawabunko827@icloud.com>
2020-01-22 15:04:11 +0900

    deploy

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -59,4 +59,4 @@
 #     auth_methods: %w(publickey password)
 #     # password: "please use keys"
 #   }
-server '<18.176.222.174>', user: 'ec2-user', roles: %w{app db web}
+server '18.176.222.174', user: 'ec2-user', roles: %w{app db web}
